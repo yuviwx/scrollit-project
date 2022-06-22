ff27d3771e55a99c9a29ade6ba2e3247479337b5
Yuval Nachman <yuviwx@gmail.com>
2022-06-22 15:45:38 +0300

    posts component css

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,14 @@
 import Search from '../components/search'
 import Posts from '../components/posts';
 import SearchStyles from '../styles/SearchStyles.css'
+import PostsStyles from '../styles/PostsStyles.css'
 
 function App() {
   return (
     <div>
       <Search style={SearchStyles}/>
       <hr />
-      <Posts />
+      <Posts style={PostsStyles}/>
     </div>
   );
 }
