bd58f27be6b4adaa9c1a60d026f705e794973cf4
Yuval Nachman <yuviwx@gmail.com>
2022-06-30 12:19:24 +0300

    refactoring code

--- src/components/posts.js
+++ src/components/posts.js
@@ -3,34 +3,20 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { loadPosts } from "../features/filters";
 import { useSelector } from "react-redux/es/exports";
-import { selectPostList } from "../features/filters"; 
+import { selectFilters } from "../features/filters"; 
 
-const chooseStyle = windowWidth => {
-    if(windowWidth > 1000){
-        return 3
-    }else if(windowWidth > 600){
-        return 2
-    }
-    return 1
-}
 
 export default function Posts() {
-    let style = chooseStyle(window.outerWidth)
-    const dispatch = useDispatch()
+    const {postList, searchTerm, searchLimit, sortBy} = useSelector(selectFilters);
+    const dispatch = useDispatch();
     useEffect(() => {
-        dispatch(loadPosts())
-    },[dispatch])
-
-    useEffect(() => {
-        style = chooseStyle(window.outerWidth)
-    }, [window.outerWidth])
-
-    const postList = useSelector(selectPostList)
+        dispatch(loadPosts(searchTerm, searchLimit, sortBy))
+    },[])
 
     return (
     <main>
         {
-            postList && postList.map((post, index ) => <Post key={index} Data={post} style={style} video={post.media}/>)
+            postList && postList.map((post, index ) => <Post key={index} Data={post} />)
         }
     </main>
     );
